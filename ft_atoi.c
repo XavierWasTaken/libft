03322348e9d42f00370daed7aacc4357079ad3fe
Pedro Miguel Da Rocha Xavier <pda-roch@c1r9s7.42porto.com>
2024-05-17 19:14:11 +0100

    libft final

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,13 +6,13 @@
 /*   By: pda-roch <pda-roch@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 17:26:04 by pda-roch          #+#    #+#             */
-/*   Updated: 2024/05/17 16:12:41 by pda-roch         ###   ########.fr       */
+/*   Updated: 2024/05/17 19:03:14 by pda-roch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(const char *str)
+int	ft_atoi(const char *nptr)
 {
 	int	i;
 	int	sign;
@@ -21,18 +21,18 @@ int	ft_atoi(const char *str)
 	i = 0;
 	nb = 0;
 	sign = 1;
-	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+	while (nptr[i] == 32 || (nptr[i] >= 9 && nptr[i] <= 13))
 		i++;
-	if (str[i] == '-')
+	if (nptr[i] == '-')
 	{
 		sign = -1;
 		i++;
 	}
-	else if (str[i] == '+')
+	else if (nptr[i] == '+')
 		i++;
-	while (str[i] >= '0' && str[i] <= '9')
+	while (nptr[i] >= '0' && nptr[i] <= '9')
 	{
-		nb = (nb * 10) + (str[i] - '0');
+		nb = (nb * 10) + (nptr[i] - '0');
 		i++;
 	}
 	return (nb * sign);
--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,21 +6,21 @@
 /*   By: pda-roch <pda-roch@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 17:21:46 by pda-roch          #+#    #+#             */
-/*   Updated: 2024/04/26 18:02:26 by pda-roch         ###   ########.fr       */
+/*   Updated: 2024/05/17 18:54:46 by pda-roch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_memcmp(const void *sl, const void *s, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t			i;
 
 	i = 0;
 	while (i < n)
 	{
-		if (*(unsigned char *)(sl + i) != *(unsigned char *)(s + i))
-			return (*(unsigned char *)(sl + i) - *(unsigned char *)(s + i));
+		if (*(unsigned char *)(s1 + i) != *(unsigned char *)(s2 + i))
+			return (*(unsigned char *)(s1 + i) - *(unsigned char *)(s2 + i));
 		i++;
 	}
 	return (0);
